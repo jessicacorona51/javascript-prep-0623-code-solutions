ac37d07d8ec0c651237dd7bfa583515da5fe4a5e
Jessica Corona <jessicacorona51@yahoo.com>
2023-06-23 05:02:51 +0000

    Completed prep-javascript-operators-and-expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 1;
+const number2 = 2;
+const product = number1 * number2;
+console.log('value of product:', product);
+console.log('type of product:', typeof product);
+
+const charge = 2;
+const payment = 1;
+const amountRemaining = payment - charge;
+console.log('value of amountRemaining:', amountRemaining);
+console.log('type of amountRemaining:', typeof amountRemaining);
+
+const test = 50;
+const assignments = 60;
+const final = 70;
+const grade = (50 + 60 + 70) / 3;
+console.log('value of grade:', grade);
+console.log('type of grade:', typeof grade);
+
+const firstName = "Jessica";
+const lastName = " Corona";
+const fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+const pH = 5;
+const isAcidic = 5 < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+
+const numberOfSoldiers = 10;
+const isSparta = numberOfSoldiers === 300;
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+const nickname = fullName + " is the Goat"
+console.log('value of nickname:', nickname);
+console.log('type of nickname:', typeof nickname);
---
+++ prep-javascript-operators-and-expressions/prep-javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,43 @@
+# prep-javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What data type is returned by an arithmetic operation?
+  an arithmetic operation returns a single numeric value.
+- What is string concatenation?
+  When you add one string to another
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+  The '+' operator produces the sum of numeric operands or string concatenation
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+  It returns true or false
+- What does the `+=` "plus-equals" operator do?
+  The addition assignment performs addition and assigns it to the left operand.
+
+
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
